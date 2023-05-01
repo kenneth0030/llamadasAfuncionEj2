7108ec817fb08ab7238eab227a09b312b902a017
kenneth pegueros <kenneth_183@hotmail.com>
2023-05-01 16:40:54 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/llamadasafuncionej2/LlamadasAfuncionEj2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.llamadasafuncionej2;
+
+/**
+ *
+ * @author kenne
+ */
+public class LlamadasAfuncionEj2 {
+
+    public void division(int a, int b) {
+        int resultado = a/b;
+        System.out.println("el resultado =" + resultado );
+    }
+
+    public static void main(String[] args) {
+        LlamadasAfuncionEj2 op = new LlamadasAfuncionEj2();
+        op.division(3, 2);
+    }
+}
